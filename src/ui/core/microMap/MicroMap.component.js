b4847f1ee2594c921914637b706201a7134e4184
Stuart Anderson <andest01@gmail.com>
2016-12-29 14:18:57 -0600

    Continue updating micromap

--- src/ui/core/microMap/MicroMap.component.js
+++ src/ui/core/microMap/MicroMap.component.js
@@ -1,86 +1,19 @@
 import React, { PropTypes } from 'react'
 import classes from './MicroMap.scss'
-import * as colors from 'ui/core/Colors'
-import * as d3Geo from 'd3-geo'
-// import * as d3Path from 'd3-path'
-import { getProjectionFromFeature } from 'ui/core/header/minimap/svgMinimap/SvgMap.component'
+import * as Micromap from './Micromap'
 
 // calling getBoundingClientRect in WebKit is
 // excruciatingly slow. CACHE IT FOR PERFORMANCE!
 let boundingRectangleCache = null
-const TAU = Math.PI * 2
-const LINE_WIDTH = 0.5
-const STREAM_WIDTH = LINE_WIDTH
-const TROUT_SECTION_WIDTH = LINE_WIDTH * 1.5
-const PUBLIC_SECTION = LINE_WIDTH * 2.6
-const END_POINT_SIZE = LINE_WIDTH * 2
+
 const SneezeGuardComponent = React.createClass({
   propTypes: {
     streamObject: PropTypes.object.isRequired,
     id: PropTypes.string.isRequired
   },
 
-  // https://www.html5rocks.com/en/tutorials/canvas/hidpi/
-  setUpCanvas (width, height) {
-    let context = this.canvasElement.getContext('2d')
-    this.canvasElement.height = height
-    this.canvasElement.width = width
-
-    let devicePixelRatio = window.devicePixelRatio || 1
-    let backingStoreRatio = context.webkitBackingStorePixelRatio ||
-                        context.mozBackingStorePixelRatio ||
-                        context.msBackingStorePixelRatio ||
-                        context.oBackingStorePixelRatio ||
-                        context.backingStorePixelRatio || 1
-    let ratio = devicePixelRatio / backingStoreRatio
-    if (devicePixelRatio !== backingStoreRatio) {
-      let canvas = this.canvasElement
-      let oldWidth = canvas.width
-      let oldHeight = canvas.height
-
-      canvas.width = oldWidth * ratio
-      canvas.height = oldHeight * ratio
-
-      canvas.style.width = oldWidth + 'px'
-      canvas.style.height = oldHeight + 'px'
-
-      // now scale the context to counter
-      // the fact that we've manually scaled
-      // our canvas element
-      context.scale(ratio, ratio)
-    }
-
-    return context
-  },
-
-  renderCanvas (streamObject) {
-    this.projection = getProjectionFromFeature(streamObject.circle, this.dimensions)
-    this.geoPath = d3Geo.geoPath()
-      .projection(this.projection)
-      .pointRadius(END_POINT_SIZE)
-      .context(this.canvasContext)
-    let { geoPath, canvasContext } = this
-    // the first thing we do is clear the context right away.
-    canvasContext.clearRect(0, 0, this.width, this.height)
-    // render petri dish
-    // this.renderPetriDish(canvasContext, colors.MoodyGray)
-
-    // render stream
-    this.renderStream(geoPath, canvasContext, streamObject.stream, colors.StreamGray, STREAM_WIDTH)
-
-    // render sections
-    streamObject.sections.forEach(section => {
-      this.renderStream(geoPath, canvasContext, section, colors.StreamBlue, TROUT_SECTION_WIDTH)
-    })
-
-    // render public sections
-    streamObject.palSections.forEach(section => {
-      this.renderStream(geoPath, canvasContext, section, colors.PalGreen, PUBLIC_SECTION)
-    })
-  },
-
   componentWillUpdate (nextProps) {
-    this.fireRenderToCanvas(nextProps.streamObject, true)
+    this.fireRenderToCanvas(nextProps.streamObject)
   },
 
   componentDidMount () {
@@ -93,80 +26,27 @@ const SneezeGuardComponent = React.createClass({
     let { height, width } = boundingRectangleCache
     this.width = width
     this.height = height
-    this.canvasContext = this.setUpCanvas(this.width, this.height)
+    let devicePixelRatio = window.devicePixelRatio || 1
+    this.canvasContext = Micromap.setUpCanvas(this.canvasElement, this.width, this.height, devicePixelRatio)
     this.dimensions = {
       width,
       height,
-      radius: Math.min(width, height) * 0.5,
-      buffer: 1
+      radius: (Math.min(width, height) - 3) * 0.5,
+      buffer: 3,
+      arcCompressionRatio: 0.90,
+      rotatePhase: Math.PI / 2
     }
 
     // it's polite to save our canvas style here.
     // draw a big rectangle to clear our canvas.
-    this.canvasContext.fillStyle = colors.MoodyGray
-    this.canvasContext.save()
+    // this.canvasContext.fillStyle = colors.MoodyGray
+    // this.canvasContext.save()
     let offset = (Math.random() * 5) + 20
-    setTimeout(() => this.fireRenderToCanvas(this.props.streamObject, true), offset)
+    setTimeout(() => this.fireRenderToCanvas(this.props.streamObject), offset)
   },
 
-  fireRenderToCanvas (streamObject, delay = true) {
-    if (delay === false) {
-      this.renderCanvas(streamObject)
-      return
-    }
-
-    this.renderCanvas(streamObject)
-  },
-
-  renderStream (path, context, geoJson, color = 'red', thickness = 1) {
-    if (path == null) {
-      return
-    }
-
-    if (context == null) {
-      return
-    }
-    context.lineWidth = thickness
-    context.strokeStyle = color
-    context.beginPath()
-    path(geoJson)
-    context.stroke()
-    context.restore()
-  },
-
-  renderArc (arcPath, context, geoJson, color = 'red', thickness = 1) {
-
-  },
-
-  renderPointOnStream (projection, context, { coordinates }, color = 'red', radius = 1) {
-    if (projection == null) {
-      return
-    }
-
-    if (context == null) {
-      return
-    }
-    context.save()
-    context.fillStyle = color
-    let canvasCoordiantes = projection(coordinates)
-    context.lineWidth = 1
-    context.strokeStyle = color
-    context.globalAlpha = 0.4
-    context.beginPath()
-    context.arc(canvasCoordiantes[0], canvasCoordiantes[1], radius, 0, TAU, true)
-    context.stroke()
-    context.restore()
-  },
-
-  renderPetriDish (context, color) {
-    context.fillStyle = color
-    context.lineWidth = 1
-    context.strokeStyle = color
-    context.beginPath()
-    context.arc(this.width * 0.5, this.height * 0.5, (this.width * 0.5) - 3, 0, TAU, true)
-    context.fill()
-    context.stroke()
-    context.restore()
+  fireRenderToCanvas (streamObject) {
+    Micromap.drawStreamToCanvas(this.canvasContext, streamObject, this.dimensions)
   },
 
   componentWillUnmount () {
---
+++ src/ui/core/microMap/Micromap.js
@@ -0,0 +1,192 @@
+import { getProjectionFromFeature } from '../header/minimap/svgMinimap/GetProjectionFromFeature'
+import * as colors from '../../core/Colors'
+import * as d3Geo from 'd3-geo'
+import * as d3Path from 'd3-path'
+// let window = window || null
+const TAU = Math.PI * 2
+const LINE_WIDTH = 0.5 * 4
+const STREAM_WIDTH = LINE_WIDTH
+const TROUT_SECTION_WIDTH = LINE_WIDTH * 1.5
+const PUBLIC_SECTION = LINE_WIDTH * 2.6
+const END_POINT_SIZE = LINE_WIDTH * 2
+
+const getBackingStoreRatio = (context) => {
+  return context.webkitBackingStorePixelRatio ||
+    context.mozBackingStorePixelRatio ||
+    context.msBackingStorePixelRatio ||
+    context.oBackingStorePixelRatio ||
+    context.backingStorePixelRatio || 1
+}
+
+// https://www.html5rocks.com/en/tutorials/canvas/hidpi/
+export const setUpCanvas = (canvasElement, width, height, devicePixelRatio = 1) => {
+  let context = canvasElement.getContext('2d')
+  canvasElement.height = height
+  canvasElement.width = width
+
+  let backingStoreRatio = getBackingStoreRatio(context)
+  let ratio = devicePixelRatio / backingStoreRatio
+  if (devicePixelRatio !== backingStoreRatio) {
+    let canvas = canvasElement
+    let oldWidth = canvas.width
+    let oldHeight = canvas.height
+
+    canvas.width = oldWidth * ratio
+    canvas.height = oldHeight * ratio
+
+    canvas.style.width = oldWidth + 'px'
+    canvas.style.height = oldHeight + 'px'
+
+    // now scale the context to counter
+    // the fact that we've manually scaled
+    // our canvas element
+    context.scale(ratio, ratio)
+  }
+
+  return context
+}
+
+export const drawBackground = (canvasContext, dimensions, color) => {
+  let { width, height } = dimensions
+  canvasContext.clearRect(0, 0, width, height)
+  canvasContext.fillStyle = color
+  canvasContext.fillRect(0, 0, width, height)
+}
+
+const renderStreams = (streamObject, canvasContext, dimensions) => {
+  let projection = getProjectionFromFeature(streamObject.circle, dimensions)
+  let geoPath = d3Geo.geoPath()
+    .projection(projection)
+    .pointRadius(END_POINT_SIZE)
+    .context(canvasContext)
+
+  // render stream
+  renderStream(geoPath, canvasContext, streamObject.stream, colors.StreamGray, STREAM_WIDTH)
+
+  // // render sections
+  streamObject.sections.forEach(section => {
+    renderStream(geoPath, canvasContext, section, colors.StreamBlue, TROUT_SECTION_WIDTH)
+  })
+
+  // render public sections
+  streamObject.palSections.forEach(section => {
+    renderStream(geoPath, canvasContext, section, colors.PalGreen, PUBLIC_SECTION)
+  })
+}
+
+const renderStream = (path, context, geoJson, color = 'red', thickness = 1) => {
+  if (path == null) {
+    return
+  }
+
+  if (context == null) {
+    return
+  }
+  context.lineWidth = thickness
+  context.lineJoin = 'round'
+  context.strokeStyle = color
+  context.beginPath()
+  path(geoJson)
+  context.stroke()
+}
+
+const renderArc = (arcPath, context, geoJson, color = 'red', thickness = 1) => {
+
+}
+
+const renderPointOnStream = (projection, context, { coordinates }, color = 'red', radius = 1) => {
+  if (projection == null) {
+    return
+  }
+
+  if (context == null) {
+    return
+  }
+  context.save()
+  context.fillStyle = color
+  let canvasCoordiantes = projection(coordinates)
+  context.lineWidth = 1
+  context.strokeStyle = color
+  context.globalAlpha = 0.4
+  context.beginPath()
+  context.arc(canvasCoordiantes[0], canvasCoordiantes[1], radius, 0, TAU, true)
+  context.stroke()
+}
+
+export const renderPetriDish = (context, dimensions, color) => {
+  let { width, height } = dimensions
+  context.fillStyle = color
+  context.lineWidth = 1
+  context.strokeStyle = color
+  context.beginPath()
+  context.arc(width * 0.5, height * 0.5, (width * 0.5) - 3, 0, TAU, true)
+  context.fill()
+  context.stroke()
+}
+
+export const drawStreamToCanvas = (canvasContext, streamObject, dimensions) => {
+  renderStreams(streamObject, canvasContext, dimensions)
+  // drawRingToCanvas(canvasContext, streamObject, dimensions)
+  return canvasContext
+}
+
+export const drawRingToCanvas = (canvasContext, streamObject, dimensions) => {
+  renderRings(streamObject, canvasContext, dimensions)
+  return canvasContext
+}
+
+const renderRings = (streamObject, canvasContext, dimensions) => {
+  renderStreamRing(streamObject, canvasContext, dimensions)
+  renderTroutStreamSectionRings(streamObject, canvasContext, dimensions)
+  renderPublicSectionRings(streamObject, canvasContext, dimensions)
+}
+
+export const renderStreamRing = (streamObject, canvasContext, dimensions) => {
+  let length = streamObject.stream.properties.length_mi
+  let start = 0
+  let stop = length
+  let ring = { start, stop, length }
+  renderRing(ring, canvasContext, dimensions, colors.StreamGray, STREAM_WIDTH)
+}
+
+export const renderTroutStreamSectionRings = (streamObject, canvasContext, dimensions) => {
+  let streamLength = streamObject.stream.properties.length_mi
+  streamObject.sections.forEach((section, index) => {
+    let ring = {
+      start: section.properties.start,
+      stop: section.properties.stop,
+      length: streamLength
+    }
+
+    renderRing(ring, canvasContext, dimensions, colors.StreamBlue, TROUT_SECTION_WIDTH)
+  })
+}
+
+export const renderPublicSectionRings = (streamObject, canvasContext, dimensions) => {
+  let streamLength = streamObject.stream.properties.length_mi
+  streamObject.palSections.forEach((section, index) => {
+    let ring = {
+      start: section.properties.start,
+      stop: section.properties.stop,
+      length: streamLength
+    }
+
+    renderRing(ring, canvasContext, dimensions, colors.PalGreen, PUBLIC_SECTION)
+  })
+}
+
+const renderRing = ({ start, stop, length }, context, dimensions, color, thickness = STREAM_WIDTH) => {
+  let { width, height, radius, arcCompressionRatio, rotatePhase } = dimensions
+  let data = [start, stop].map(mileOffset => {
+    let normalizedOffset = mileOffset / length
+    let normalizedArcLength = TAU * arcCompressionRatio
+    let arcOffset = (normalizedOffset * normalizedArcLength) - rotatePhase
+    return arcOffset
+  })
+  context.beginPath()
+  context.lineWidth = thickness
+  context.lineJoin = 'round'
+  context.strokeStyle = color
+  context.arc(width / 2, height / 2, radius, data[0], data[1])
+  context.stroke()
+}

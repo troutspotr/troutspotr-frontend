0b7abf5bae1a97790b9126b8a8b268f97fddf1b9
Stuart Anderson <andest01@gmail.com>
2017-01-01 19:01:41 -0600

    stupid lint...

--- bin/server.js
+++ bin/server.js
@@ -4,4 +4,4 @@ const debug = require('debug')('app:bin:server')
 const port = config.server_port
 
 server.listen(port)
-// debug(`Server is now running at http://localhost:${port}.`)
+debug(`Server is now running at http://localhost:${port}.`)

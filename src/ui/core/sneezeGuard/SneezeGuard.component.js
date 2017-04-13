2508871c51e8fcb8dd5e8d890817841e85508576
Stuart Anderson <andest01@gmail.com>
2017-04-12 19:05:22 -0500

    BACK AGAIN!

---
+++ src/ui/core/sneezeGuard/SneezeGuard.component.js
@@ -0,0 +1,19 @@
+import React, { PropTypes, Component } from 'react'
+import classes from './SneezeGuard.scss'
+class SneezeGuardComponent extends Component {
+  onClick = () => {
+    if (this.props.close) {
+      this.props.close()
+    }
+  }
+
+  render () {
+    return (<span onClick={this.onClick} className={classes.sneezeGuard} />)
+  }
+}
+
+SneezeGuardComponent.propTypes = {
+  close: PropTypes.func
+}
+
+export default SneezeGuardComponent

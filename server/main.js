280d047e56dd36e8ccce7a23ba4408d668678958
Stuart Anderson <andest01@gmail.com>
2017-04-30 14:57:51 -0500

    Attempts at redirection to https

--- server/main.js
+++ server/main.js
@@ -11,8 +11,21 @@ const createSeoInterceptor = require('./Interceptor')
 const GetSiteDictionary = require('./GetSiteDictionary')
 const _ = require('lodash')
 app.use(compress())
+var env = process.env.NODE_ENV || 'development'
+var forceSsl = function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''))
+  }
+  return next()
+}
 
 const createServer = function (dictionary, app) {
+  app.use(function () {
+    if (env === 'production') {
+      app.use(forceSsl)
+    }
+  })
+
   var seoInterceptor = createSeoInterceptor(dictionary)
   app.use(seoInterceptor)
 

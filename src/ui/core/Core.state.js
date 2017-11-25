5f7a570cd3f0535f27b02d9c0f498479d80c68a4
stuart <andest01@gmail.com>
2017-11-25 17:52:15 -0600

    add comma

--- src/ui/core/Core.state.js
+++ src/ui/core/Core.state.js
@@ -172,7 +172,7 @@ const initialState = {
   'hasSeenIntroScreen': false,
   'hasSeenTermsOfService': false,
   'hasSeenPrivacyPolicy': false,
-  'hasAgreedToTerms': true //getHasAgreedToTerms(),
+  'hasAgreedToTerms': true, //getHasAgreedToTerms(),
 }
 
 export default function counterReducer (state = initialState, action) {

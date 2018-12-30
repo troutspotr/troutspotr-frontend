2d388a01bbd9f000652804ad82ce8893b4052b66
Stu Anderson <andest01@gmail.com>
2018-12-30 02:20:32 -0600

    reduce regs into single line, round nicely. fix layout.

--- src/ui/routes/map/overlays/regulations/RegulationsOverlay.selectors.ts
+++ src/ui/routes/map/overlays/regulations/RegulationsOverlay.selectors.ts
@@ -14,9 +14,49 @@ export const selectedStreamRestrictions = createSelector(
   isRegulationsShownSelector,
   selectedStreamObjectSelector,
   (isShown, streamObject): ReadonlyArray<IRestrictionComponent> => {
-    return isShown === false ? null : streamObject.restrictions.map(convertStreamRegulationsToRestrictionProps)
+    if (isShown === false) {
+      return null
+    }
+
+    const RestrictionHash: { [key: string]: RestrictionFeature } = {}
+    const restrictionSummary = streamObject.restrictions.reduce((dictionary, restriction:RestrictionFeature) => {
+      const dictionaryValue = dictionary[restriction.properties.restriction_id]
+      const sectionLengthInMiles = (restriction.properties.stop - restriction.properties.start)
+      if (dictionaryValue == null) {
+        dictionary[restriction.properties.restriction_id] = {
+          ...restriction,
+          properties: {
+            ...restriction.properties,
+            length: sectionLengthInMiles
+          }
+        }
+        return dictionary
+      }
+
+      dictionary[restriction.properties.restriction_id] = {
+        ...dictionaryValue,
+        properties: {
+          ...dictionaryValue.properties,
+          length: dictionaryValue.properties.length + sectionLengthInMiles,
+        }
+      }
+
+      return dictionary
+
+    }, RestrictionHash)
+    
+    return Object.values(restrictionSummary).map(convertStreamRegulationsToRestrictionProps)
   }
 )
+
+export const convertLengthToFriendlyString = (length:number):string =>{
+  const digitsToRound = length < 10
+    ? 2
+    : length < 100
+      ? 1
+      : 0
+  return Number(Number(length).toFixed(digitsToRound)).toString()
+}
 
 export const convertStreamRegulationsToRestrictionProps = (restriction: RestrictionFeature): IRestrictionComponent => {
   // assume restriction is not null
@@ -24,7 +64,7 @@ export const convertStreamRegulationsToRestrictionProps = (restriction: Restrict
     color: restriction.properties.color as 'red' | 'yellow' | 'blue' | 'green' | 'white' | 'blueGray',
     pattern: 'stipple' as 'solid' | 'stipple' | 'dot',
     text: restriction.properties.restriction.legalText,
-    length: '3.56 mi',
+    length: `${convertLengthToFriendlyString(restriction.properties.length)} mi`,
     hollow: true,
     heightMultiplier: 0.7,
   }

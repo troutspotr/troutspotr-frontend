5656ec901f944aed5da2eacb48d1158662c0ddfb
Stu <1424223+andest01@users.noreply.github.com>
2019-01-20 10:01:16 -0600

    remove tint on deep zoom except when offline (#229)

--- src/ui/routes/map/MapboxGlMap/styles/Pal.layers.ts
+++ src/ui/routes/map/MapboxGlMap/styles/Pal.layers.ts
@@ -4,13 +4,28 @@ import { ILayerProperties } from './ICreateLayer'
 export const PAL_LAYER_ID = 'pal_geom_layer'
 export const PAL_BORDER_LAYER_ID = 'pal_border_layer'
 
-const darkModePalLayerFillOpacityStops = [[7, 0], [9, 0.05], [12, 0.1], [14, 0.15], [16, 0.25], [18, 0.1]]
-const lightModePalLayerFillOpacityStops = [[6, 0.0], [7, 0.3], [9, 0.3], [12, 0.4], [14, 0.5], [15, 0.3], [17, 0.2], [18, 0.1]]
+const darkModePalLayerFillOpacityStops = () => [[4, 0.05], [9, 0.05], [12, 0.1], [14, 0.15], [16, 0.25], [16.7, 0.1], [17, 0]]
+const lightModePalLayerFillOpacityStops = () => [[4, 0.05], [7, 0.3], [9, 0.3], [12, 0.4], [14, 0.5], [15, 0.3], [16.7, 0.1], [17, 0]]
+
+export const getOpacityStops = (layerProps: ILayerProperties): number[][] => {
+  const opacityStops = layerProps.isHighContrastEnabled
+  ? lightModePalLayerFillOpacityStops()
+  : darkModePalLayerFillOpacityStops()
+
+  if (layerProps.isOnline === false) {
+    const updatedStops = [...opacityStops]
+    // update the last two stops
+    const numberOfStops = updatedStops.length
+    updatedStops[numberOfStops - 1][1] = 0.1
+    return updatedStops
+  }
+  
+  return opacityStops
+}
 export const createPalLayer = (layerProps: ILayerProperties, sourceId: string): Layer[] => {
   const { pallete } = layerProps
-  const opacityStops = layerProps.isHighContrastEnabled
-    ? lightModePalLayerFillOpacityStops
-    : darkModePalLayerFillOpacityStops
+  const opacityStops = getOpacityStops(layerProps)
+  
   const streamStyle: Layer = {
     id: PAL_LAYER_ID,
     type: 'fill',
@@ -56,4 +71,3 @@ export const createPalBorderLayer = (layerProps: ILayerProperties, sourceId: str
 
   return [streamStyle]
 }
-

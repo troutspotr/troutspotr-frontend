3cc9209926ee567ce47216b1895744fd543b2c43
andest01 <andest01@gmail.com>
2019-02-03 21:17:44 -0600

    use real names

--- src/ui/routes/map/overlays/loading/Loading.selectors.ts
+++ src/ui/routes/map/overlays/loading/Loading.selectors.ts
@@ -1,10 +1,14 @@
 import { createSelector } from 'reselect'
+import { selectedStateSelector, selectedRegionSelector } from 'ui/core/Core.selectors';
 
 export const loadingMessageTitleSelector = createSelector(
-  [() => null],
-  () => {
-    const regionName = 'Region Name'
-    const stateName = 'MN'
+  [selectedStateSelector, selectedRegionSelector],
+  (selectedState, selectedRegion) => {
+    if (selectedState == null || selectedRegion == null) {
+      return ''
+    }
+    const regionName = selectedRegion.properties.long_name
+    const stateName = selectedState.properties.short_name.toUpperCase()
     return `Loading ${regionName}, ${stateName}...`
   }
 )
